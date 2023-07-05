0c31e2427a625cdc1b451724a9d2e28be47ae700
bipeti <peter.bihari@gmail.com>
2023-07-05 15:08:24 +0200

    test again

---
+++ src/components/utils/myConsts.tsx
@@ -0,0 +1,10 @@
+/* emailjs.send constans */
+export const MY_SERVICE_ID = "service_5aopvtj";
+export const MY_TEMPLATE_ID = "template_tkqfwuh";
+export const MY_PUBLIC_KEY = "spbia2xqnaePNp_0N";
+
+/* database path  */
+export const DB_PATH =
+    "https://pizzeria-39338-default-rtdb.europe-west1.firebasedatabase.app";
+
+/* other keys*/
